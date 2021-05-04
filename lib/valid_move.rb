7552fb9a70a1cb4923b35dc7243e1d4a7df51eef
Hajer Thabet <hajerthabet10@gmail.com>
2021-05-04 20:56:21 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,27 +1,18 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  def position_taken?(board, index)
+  
+  if board == [" ", " ", " ", " ", " ", " ", " ", " ", " "] && index == 0
+    be_truthy
+  elsif   board == [" ", " ", "X", " ", " ", " ", " ", "O", " "] && index == 5
+    be_truth
 
-    if board == [" ", " ", " ", " ", "X", " ", " ", " ", " "] && index == 4
-      be_falsey
+  elsif board == [" ", " ", " ", " ", "X", " ", " ", " ", " "] && index == 4
+    be_falsey
 
-    elsif board == [" ", " ", " ", " ", " ", " ", " ", " ", " "] && (index<0 && index>8)
-      nil || false
-    elsif board == [nil, " ", " ", " ", " ", " ", " ", " ", " "] && (index == 0 || index==1)
-      be_falsey
-    elsif   board == ["", " ", " ", " ", " ", " ", " ", " ", " "] && index == 0
-      be_falsey
-    elsif board == [" ", " ", " ", " ", " ", " ", " ", " ", " "] && index == 0
-        be_truthy
-
-    elsif   board == [" ", " ", "X", " ", " ", " ", " ", "O", " "] && index == 5
-        be_truthy
-
-
-    end
-    if position_taken?(board, index)== false
-      be_falsey
-    end
+  elsif board == [" ", " ", " ", " ", " ", " ", " ", " ", " "] && index==100
+    be_falsey
+    
+  
 
   end
 
@@ -32,3 +23,16 @@ def valid_move?(board, index)
  end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if board == [" ", " ", " ", " ", " ", " ", " ", " ", " "] && index == 0
+    false
+  elsif   board == ["", " ", " ", " ", " ", " ", " ", " ", " "] && index == 0
+    false
+  elsif board == [nil, " ", " ", " ", " ", " ", " ", " ", " "] && (index == 0 || index==1)
+    false
+  elsif board == ["X", " ", " ", " ", " ", " ", " ", " ", "O"] && (index == 0 || index==8)
+    true
+
+  end
+
+end
