e026e194d0e148f97b3c87915638bca4fffae239
Hajer Thabet <hajerthabet10@gmail.com>
2021-05-04 20:12:43 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -16,9 +16,9 @@ def valid_move?(board, index)
 
   end
   if position_taken?(board, index)== false
-    valid_move?(board, index)==false
+    valid_move?(==false
   elsif position_taken?(board, index)== true 
-    valid_move?(board, index)==true
+    valid_move?==true
   end
 
 
